8e9910c8215002a113b3097390c11cd6b11e0165
Thắng Nguỵ <bboy114crew@gmail.com>
2021-10-03 22:42:42 +0700

    Solve Fox and Names

--- ds1.py
+++ ds1.py
@@ -42,55 +42,123 @@
 # else:
 #   print("Sandro fails.")
 
-# Hierarchy
-import queue
-N, K = list(map(int, input().split()))
-graph = [[] for i in range(N + 1)]
-boss = dict()
-result = []
+# # Hierarchy
+# import queue
+# N, K = list(map(int, input().split()))
+# graph = [[] for i in range(N + 1)]
+# boss = dict()
+# result = []
 
-for u in range(1, K + 1):
-  arr = list(map(int, input().split()))
-  first, rest = arr[0], arr[1:]
-  for v in rest:
-    graph[u].append(v)
+# for u in range(1, K + 1):
+#   arr = list(map(int, input().split()))
+#   first, rest = arr[0], arr[1:]
+#   for v in rest:
+#     graph[u].append(v)
+
+# def topological_sort_using_kahn(graph, result):
+#   in_degree = [0 for _ in range(N + 1)]
+#   zero_in_degree = queue.Queue()
+#   index = 0
+
+#   # Tính bậc vào của từng đỉnh đỉnhh
+#   for u in range(1, N + 1):
+#     for v in graph[u]:
+#       in_degree[v] += 1
+
+#   # Tìm các đỉnh có bậc vào là 0
+#   for i in range(1, N + 1):
+#     if (in_degree[i] == 0):
+#       zero_in_degree.put(i)
+  
+#   # Thực hiện thuật toán Kahn
+#   while not zero_in_degree.empty():
+#     u = zero_in_degree.get()
+#     result.append(u)
+
+#     # Mapping u with position in result
+#     if len(result) == 1:
+#       boss[u] = 0
+#       index += 1
+#     else:
+#       boss[u] = result[index - 1]
+#       index += 1
+
+#     for v in graph[u]:
+#       in_degree[v] -= 1
+#       if (in_degree[v] == 0):
+#         zero_in_degree.put(v)
+  
+#   sorted_boss = sorted(boss.items())
+
+#   for r in sorted_boss:
+#     print(r[1])
+
+# topological_sort_using_kahn(graph, result)
+
+# Fox and Names
+import queue
+ALPHABETS = "abcdefghijklmnopqrstuvwxyz"
+alpha_dict = dict()
+
+for i in range(len(ALPHABETS)):
+  alpha_dict[ALPHABETS[i]] = i
+
+n = int(input())
+
+graph = [[] for _ in range(len(ALPHABETS))]
+result = []
+authors = []
 
 def topological_sort_using_kahn(graph, result):
-  in_degree = [0 for _ in range(N + 1)]
+  in_degree = [0 for _ in range(len(ALPHABETS))]
   zero_in_degree = queue.Queue()
-  index = 0
 
   # Tính bậc vào của từng đỉnh đỉnhh
-  for u in range(1, N + 1):
+  for u in range(0, len(ALPHABETS)):
     for v in graph[u]:
       in_degree[v] += 1
 
   # Tìm các đỉnh có bậc vào là 0
-  for i in range(1, N + 1):
+  for i in range(0, len(ALPHABETS)):
     if (in_degree[i] == 0):
       zero_in_degree.put(i)
   
   # Thực hiện thuật toán Kahn
   while not zero_in_degree.empty():
     u = zero_in_degree.get()
-    result.append(u)
-
-    # Mapping u with position in result
-    if len(result) == 1:
-      boss[u] = 0
-      index += 1
-    else:
-      boss[u] = result[index - 1]
-      index += 1
+    result.append(chr(u + 97))
 
     for v in graph[u]:
       in_degree[v] -= 1
       if (in_degree[v] == 0):
         zero_in_degree.put(v)
+
+  if (len(result) < len(ALPHABETS)):
+    return False
   
-  sorted_boss = sorted(boss.items())
+  return True
 
-  for r in sorted_boss:
-    print(r[1])
+for i in range(n):
+  author = input()
+  authors.append(author)
 
-topological_sort_using_kahn(graph, result)
+current_index = 0
+
+while current_index < n - 1:
+  current_au = authors[current_index]
+  next_au = authors[current_index + 1]
+  
+  loop_count = min(len(current_au), len(next_au))
+  for i in range(loop_count):
+    if current_au[i] != next_au[i]:
+      graph[alpha_dict[current_au[i]]].append(alpha_dict[next_au[i]])
+      break
+    if i == len(next_au) - 1 and len(next_au) < len(current_au):
+      print("Impossible")
+      exit()
+  current_index += 1
+
+if (topological_sort_using_kahn(graph, result)):
+  print("".join(str(r) for r in result))
+else:
+  print("Impossible")
