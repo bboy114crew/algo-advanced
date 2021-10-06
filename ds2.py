ff01d18cbae6774f422245221bd5da4d10bd7a90
Thắng Nguỵ <bboy114crew@gmail.com>
2021-10-07 00:36:36 +0700

    Add Aish And XOR

--- ds2.py
+++ ds2.py
@@ -6,8 +6,14 @@ S = list(map(int, input().split()))
 
 f = [0 for _ in range(N)]
 
-for i in range(1, N):
-  f[i] = f[i - 1] + (S[i] == 1)
+if N > 1:
+  if S[0] == 1:
+    f[1] = 1 + (S[1] == 1)
+  else:
+    f[1] = 0 + (S[1] == 1)
+
+  for i in range(2, N):
+    f[i] = f[i - 1] + (S[i] == 1)
 
 Q = int(input())
 
@@ -15,9 +21,18 @@ for _ in range(Q):
   L, R = list(map(int, input().split()))
   L -= 1
   R -= 1
-  numOne = f[R] - f[L-1]
+
+  if N == 1:
+    if S[0] == 1:
+      print(1, 0)
+    else:
+      print(0, 1)
+    continue
+  numOne = f[R] - f[L]
 
   numZero = (R - L + 1) - numOne
-
-  
-
+  if numOne % 2 == 1:
+    xorSum = 1
+  else:
+    xorSum = 0
+  print(xorSum, numZero)
