9b7dfa5e9bcb8bae0264fb61f9665536900fc527
Thắng Nguỵ <bboy114crew@gmail.com>
2021-10-07 20:27:38 +0700

    Solve Aish And XOR

--- ds2.py
+++ ds2.py
@@ -4,33 +4,19 @@ N = int(input())
 
 S = list(map(int, input().split()))
 
-f = [0 for _ in range(N)]
+f = [0 for _ in range(N + 1)]
 
-if N > 1:
-  if S[0] == 1:
-    f[1] = 1 + (S[1] == 1)
-  else:
-    f[1] = 0 + (S[1] == 1)
-
-  for i in range(2, N):
-    f[i] = f[i - 1] + (S[i] == 1)
+for i in range(0, N):
+  f[i + 1] = f[i] + (S[i] == 1)
 
 Q = int(input())
 
 for _ in range(Q):
   L, R = list(map(int, input().split()))
-  L -= 1
-  R -= 1
-
-  if N == 1:
-    if S[0] == 1:
-      print(1, 0)
-    else:
-      print(0, 1)
-    continue
-  numOne = f[R] - f[L]
+  numOne = f[R] - f[L - 1]
 
   numZero = (R - L + 1) - numOne
+  # 0^0^..^0^0 = 0 and 1^1 = 0 and 1^0 = 1
   if numOne % 2 == 1:
     xorSum = 1
   else:
