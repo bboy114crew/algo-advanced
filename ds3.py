d83accc3cf93c2f6132f0ccc8a690211b10cb25f
Thắng Nguỵ <bboy114crew@gmail.com>
2021-10-10 19:18:47 +0700

    Permutations of string

--- ds3.py
+++ ds3.py
@@ -1,53 +1,67 @@
-# N-Queen
-N = 4 
+# # N-Queen
+# N = 4 
 
-def print_solution(board):
-  for i in range(N):
-    for j in range(N):
-      print(board[i][j], end = ' ')
-    print()
-  print()
+# def print_solution(board):
+#   for i in range(N):
+#     for j in range(N):
+#       print(board[i][j], end = ' ')
+#     print()
+#   print()
 
-def check(board, col, row):
-  # check vertical
-  for i in range(row):
-    if board[i][col]:
-      return False
+# def check(board, col, row):
+#   # check vertical
+#   for i in range(row):
+#     if board[i][col]:
+#       return False
     
-  # check main diagonal
-  i = row
-  j = col
-  while i >= 0 and j >= 0:
-    if board[i][j]:
-      return False
-    i -= 1
-    j -= 1
+#   # check main diagonal
+#   i = row
+#   j = col
+#   while i >= 0 and j >= 0:
+#     if board[i][j]:
+#       return False
+#     i -= 1
+#     j -= 1
 
-  # check secondary diagonal
-  i = row
-  j = col
-  while j < N and i >= 0:
-    if board[i][j]:
-      return False
-    i -= 1
-    j += 1
+#   # check secondary diagonal
+#   i = row
+#   j = col
+#   while j < N and i >= 0:
+#     if board[i][j]:
+#       return False
+#     i -= 1
+#     j += 1
 
-  return True
+#   return True
 
-def N_Queen(board, row):
-  if row == N:
-    print_solution(board)
-    return True
-  for j in range(N):
-    # check if queen can be placed on chessboard
-    if check(board, row, j) == True:
-      # place this queen in board[row][j]
-      board[row][j] = 1
-      N_Queen(board, row + 1)
-      # backtracking
-      board[row][j] = 0
-  return False
+# def N_Queen(board, row):
+#   if row == N:
+#     print_solution(board)
+#     return True
+#   for j in range(N):
+#     # check if queen can be placed on chessboard
+#     if check(board, row, j) == True:
+#       # place this queen in board[row][j]
+#       board[row][j] = 1
+#       N_Queen(board, row + 1)
+#       # backtracking
+#       board[row][j] = 0
+#   return False
 
-board = [[0] * N for _ in range(N)]
+# board = [[0] * N for _ in range(N)]
 
-N_Queen(board, 0)
+# N_Queen(board, 0)
+
+# Permutations of string
+def permutations(s, l, r):
+  if l == r:
+    print(''.join(s))
+  else:
+    for i in range(l, r):
+      s[l], s[i] = s[i], s[l]
+      permutations(s, l + 1, r)
+      s[l], s[i] = s[i], s[l]
+
+s = list("ABCD")
+
+permutations(s, 0, len(s))
