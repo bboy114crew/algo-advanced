6cda9f535c23e7abb863bb19b822b516d9596a17
Nguy Van Thang <nguy.van.thang@linecorp.com>
2021-11-17 21:04:58 +0700

    Add KMP preprocess

--- ds14.py
+++ ds14.py
@@ -1,3 +1,24 @@
 # Knuth-Morris-Pratt
 
-#
+def KMP_preprocess(P, prefix):
+  prefix[0] = 0
+  i = 1
+  j = 0
+  m = len(P)
+  while i < m:
+    if P[i] == P[j]:
+      j += 1
+      prefix[i] = j
+      i += 1
+    else:
+      if j != 0:
+        j = prefix[j - 1]
+      else:
+        prefix[i] = 0
+        i += 1
+
+P = 'AAACAAAAACC'
+
+prefix = [0 for _ in range(len(P))]
+
+KMP_preprocess(P, prefix)
